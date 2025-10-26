7cf8c74c24cde54eb6113a7d7e9bf8d2a9e3fe5f
Amos Aidoo <aidooamos20@gmail.com>
2025-10-26 18:13:27 +0100

    utils and constants

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const VALID_MODES = ["air", "sea", "road", "rail"];
+
+export const REQUIRED_FIELDS = [
+  { key: "shipment_id", label: "Shipment ID" },
+  { key: "origin_address", label: "Origin Address" },
+  { key: "destination_address", label: "Destination Address" },
+  { key: "mode", label: "Mode" },
+  { key: "weight_kg", label: "Weight (kg)" },
+];
